05f1aa2711f3a21d119f1d268fa65d3855ba4815
Joseph Acquah <kobia@Josephs-MacBook-Pro.local>
2025-01-04 22:39:19 -0330

    minor update: completed fetching the profile from db, fetching server user is associated with, and redirection to the server

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,10 +1,12 @@
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
 
 
 const SetupPage = async () => {
     const profile = await initialProfile(); //get the profile
 
+    //find the server where the profile is a member
     const server = await db.server.findFirst({
         where: {
             members: {
@@ -13,7 +15,12 @@ const SetupPage = async () => {
                 }
             }
         }
-    })
+    });
+
+    //if the server exists, redirect to the server
+    if(server) {
+        redirect(`/server/${server.id}`)
+    }
     return <div>Create a Server!</div>;
 }
  
