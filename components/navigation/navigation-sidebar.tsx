23a3753a4027d6f2fe88e202c3dc9c0f23916a13
Joseph Acquah <kobia@Josephs-MacBook-Pro.local>
2025-01-17 15:53:32 -0330

    minor update: setup small vertical bar on the left for each server title , setup api requests on the navigation sidebar for retrieving images from the database, completed navigation side bar rendering out all the servers the user is a part of, setting up onclick function that will switch the view to the servers page, set up user button, moddle toggle

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,11 @@ import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import NavigationAction from "./navigation-action";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import NavigationItem from "@/components/navigation/navigation-item";
+import { ModeToggle } from "../mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export const NavigationSidebar = async () => {
     const profile = await currentProfile();
@@ -12,7 +17,7 @@ export const NavigationSidebar = async () => {
     }
 
     //get the servers that the user is a part of
-    const servers = db.server.findMany({
+    const servers = await db.server.findMany({
         where: {
             members: {
                 some: {
@@ -20,11 +25,35 @@ export const NavigationSidebar = async () => {
                 }
             }
         }
-    })
+    });
 
     return(
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1e1f22] py-3">
             <NavigationAction />
+            <Separator 
+            className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
+            />
+            <ScrollArea className="flex-1 w-full">
+               {servers.map((server) => (
+                    <div key={server.id} className="mb-4 text-center">
+                        <NavigationItem
+                        id={server.id}
+                        name={server.name}
+                        imageUrl={server.imageUrl}
+                        />
+                    </div>
+               ))}
+            </ScrollArea>
+            <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+            <ModeToggle />
+            <UserButton 
+                appearance={{
+                    elements: {
+                        avatarBox: "h-[48px] w-[48px]"
+                    }
+                }}
+            />
+            </div>
         </div>
     )
 }
