38aee630ad26989a4d92d4a9e29ba13faa4310fe
Joseph Acquah <kobia@Josephs-MacBook-Pro.local>
2025-02-26 18:22:00 -0330

    minor update: leave server functionality completed successfully, users are now able to leave the server

--- lib/current-profile.ts
+++ lib/current-profile.ts
@@ -3,7 +3,6 @@ import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/db";
 
 import { redirect } from "next/navigation";
-import { RedirectToSignIn } from "@clerk/nextjs";
 
 export const currentProfile = async () => {
     const userId  = await (await auth()).userId;
