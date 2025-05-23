4bc96e10a9e847d260ddc90bdd898b3bf45815a4
Joseph Acquah <kobia@Josephs-MacBook-Pro.local>
2025-05-23 08:22:22 -0230

    minor update: included rest of files in previous commit

--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -1,18 +1,29 @@
 import { Menu } from "lucide-react";
-import { Sheet, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { Button } from "./ui/button";
+import { NavigationSidebar } from "./navigation/navigation-sidebar";
+import { ServerSidebar } from "./server/server-sidebar";
 
 
-
-const MobileToggle = () => {
+const MobileToggle = ({
+    serverId
+}: {
+    serverId: string
+}) => {
     return ( 
         <Sheet>
             <SheetTrigger asChild>
                 {/* md:hidden makes this button show only on mobile devices */}
                 <Button variant="ghost" size="icon" className="md:hidden">
-                    <Menu />
+                    <Menu/>
                 </Button>
             </SheetTrigger>
+            <SheetContent side="left" className="p-0 flex gap-0">
+                <div className="w-[72px]">
+                    <NavigationSidebar />
+                </div>
+                <ServerSidebar serverId={serverId}/>
+            </SheetContent>
         </Sheet>
     );
 }
