fc95ac5db0179ce39de1e947cd2348b458228719
Joseph Acquah <kobia@Josephs-MacBook-Pro.local>
2025-05-28 11:35:26 -0230

    minor update: added conversation logic in lib util

--- lib/conversation.ts
+++ lib/conversation.ts
@@ -1,7 +1,20 @@
 import { db } from "@/lib/db";
 
+export const getOrCreateConversation = async (memberOneId: string, memberTwoId: string) => {
+    let conversation = await findConversation(memberOneId, memberTwoId) || await findConversation(memberTwoId, memberOneId);
+
+    if(!conversation){
+        conversation = await createNewConversation(memberOneId, memberOneId);
+    }
+
+    return conversation;
+}
+
+
 const findConversation =  async (memberOneId: string, memberTwoId: string) => {
-    return await db.conversation.findFirst({
+  try {
+
+      return await db.conversation.findFirst({
         where:{
             AND: [
                 { memberOneId: memberOneId },
@@ -20,5 +33,32 @@ const findConversation =  async (memberOneId: string, memberTwoId: string) => {
                 }
             }
         }
-    });
+    });} catch{
+        return null
+    }
+}
+
+const createNewConversation = async (memberOneId: string, memberTwoId: string) => {
+     try{
+         return await db.conversation.create({
+            data: {
+                memberOneId,
+                memberTwoId,
+            }, 
+            include: {
+                memberOne: {
+                    include: {
+                        profile: true,
+                    }
+                },
+                memberTwo: {
+                    include: {
+                        profile: true
+                    }
+                }
+            }
+         })
+     }catch{
+        return null
+     }
 }
