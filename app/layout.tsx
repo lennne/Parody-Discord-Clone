e11b41e2e9126415b84ee67274b633541181de48
Joseph Acquah <kobia@Josephs-MacBook-Pro.local>
2025-06-01 09:18:40 -0230

    minor update: provider has been set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
-import { ThemeProvider } from "@/components/providers/theme-providers";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModeToggle } from "@/components/mode-toggle";
 import { cn } from "@/lib/utils";
 import { ModalProvider } from '@/components/providers/modal-provider';
